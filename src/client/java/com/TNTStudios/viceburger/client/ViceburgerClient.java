f4fcc16291d9e862767243776207a08d98140841
TNTStudios <tntstudiosn@gmail.com>
2025-04-24 01:35:58 -0600

    1

--- src/client/java/com/TNTStudios/viceburger/client/ViceburgerClient.java
+++ src/client/java/com/TNTStudios/viceburger/client/ViceburgerClient.java
@@ -1,10 +1,17 @@
 package com.TNTStudios.viceburger.client;
 
+import com.TNTStudios.viceburger.registry.ViceburgerBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class ViceburgerClient implements ClientModInitializer {
-
     @Override
     public void onInitializeClient() {
+        // Registra tu bloque "happymeal" para que use el canal translucent
+        BlockRenderLayerMap.INSTANCE.putBlock(
+                ViceburgerBlocks.HAPPYMEAL,
+                RenderLayer.getCutout()
+        );
     }
 }
